6f2540822240baa0dd685499cc5180d0800487e0
GonDev <gonzalodvrdev@gmail.com>
2023-11-24 15:29:04 +0000

    Added ubu docker instalation

---
+++ setup-scripts/ubu-docker-setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+	"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+	sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Docker command without sudo (Optional)
+sudo usermod -aG docker $USER
+sudo systemctl stop docker
+sleep 5
+sudo systemctl start docker
+sudo chmod 666 /var/run/docker.sock
+
+=----------------------------------------------
+
+# Lazydocker
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
