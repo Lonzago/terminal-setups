31a33edd5344b770ba866a03f6666f6a57c81e7a
GonDev <gonzalodvrdev@gmail.com>
2023-11-24 17:47:38 +0000

    Fix run script

--- run-ubuntu-setup.sh
+++ run-ubuntu-setup.sh
@@ -1,37 +1,47 @@
 #!/bin/bash
 
-
-echo -n 'Install All (y/n): '
-read install_all
-
-if [ $install_all = "y" ]
-  install-basics
-  install-docker
-else
-  
-  echo 'Install Ubuntu basic packages? (y/n): '
-  read basic_packages
-  if [ "$basic_packages" = "y" ]; then
-    install-docker
-    else
-	  echo 'No Installing Ubuntu Basic packages'
-  fi
-
-  echo 'Install Ubuntu docker packages? (y/n): '
-  read docker_packages
-  if [ "$docker_packages" = "y" ]; then
-    install-docker
-	  else
-	  echo 'No Installing Ubuntu Docker packages'
-  fi  
-fi
-
-
 install-basics() {
-  echo 'Installing Ubuntu Basic packages'
+	echo 'Installing Ubuntu Basic packages'
 	sh ./setup-scripts/ubuntu/ubu-packages.sh
 }
 install-docker() {
-  echo 'Installing Ubuntu Docker packages'
+	echo 'Installing Ubuntu Docker packages'
 	sh ./setup-scripts/ubuntu/ubu-docker-setup.sh
 }
+
+#==== START SCRIPT ===========
+echo -n 'Install All (y/n): '
+read install_all
+
+if [ "$install_all" = "y" ]; then
+
+	install-basics
+	install-docker
+
+else
+
+	echo 'Install Ubuntu basic packages? (y/n): '
+	read basic_packages
+	if [ "$basic_packages" = "y" ]; then
+
+		install-basics
+
+	else
+
+		echo 'No Installing Ubuntu Basic packages'
+
+	fi
+
+	echo 'Install Ubuntu docker packages? (y/n): '
+	read docker_packages
+
+	if [ "$docker_packages" = "y" ]; then
+
+		install-docker
+
+	else
+
+		echo 'No Installing Ubuntu Docker packages'
+
+	fi
+fi
