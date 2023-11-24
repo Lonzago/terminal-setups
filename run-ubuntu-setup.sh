1744a28ebcfe4116415abc3178a94524df58521d
GonDev <gonzalodvrdev@gmail.com>
2023-11-24 16:38:38 +0000

    Create ubu run command

--- run-ubuntu-setup.sh
+++ run-ubuntu-setup.sh
@@ -5,6 +5,17 @@ read basic_packages
 
 if ["basic_packages"=="y"]; then
 	echo 'Installing Ubuntu Basic packages'
+	sudo sh ./setup-scripts/ubuntu/ubu-packages.sh
 else
 	echo 'No Installing Ubuntu Basic packages'
+fi
+
+echo 'Install Ubuntu docker packages? (y/n)'
+read docker_packages
+
+if ["docker_packages"=="y"]; then
+	echo 'Installing Ubuntu Docker packages'
+	sudo sh ./setup-scripts/ubuntu/ubu-docker-setup.sh
+else
+	echo 'No Installing Ubuntu Docker packages'
 fi
