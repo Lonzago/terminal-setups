7b13486b47f21d3b8957af99e058ad554b067772
GonDev <gonzalodvrdev@gmail.com>
2023-12-01 03:26:47 +0000

    Remove unused lines

--- zsh-configuration.sh
+++ zsh-configuration.sh
@@ -1,9 +1,6 @@
 #!bin/bash
-sudo atp install -y zsh fzf fd-find silversearcher-ag
+sudo apt install -y zsh fzf fd-find silversearcher-ag
 
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
 
 sudo rm $HOME/.zshrc && sudo cp ./config-files/zsh/zsh-config $HOME/.zshrc
-source $HOME/.zshrc
-
-zap update all
