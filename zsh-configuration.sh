00380394c8c271b5d8c05c54595f3de9532cf0de
GonDev <gonzalodvrdev@gmail.com>
2023-12-01 03:00:04 +0000

    Removed p10k configuration

--- zsh-configuration.sh
+++ zsh-configuration.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 sudo atp install -y zsh fzf fd-find silversearcher-ag
 
-sudo cp ./config-files/zsh/zsh-config $HOME/.zshrc
-sudo cp ./config-files/zsh/p10k-config $HOME/.p10k.zsh
-
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
 
+sudo rm $HOME/.zshrc && sudo cp ./config-files/zsh/zsh-config $HOME/.zshrc
 source $HOME/.zshrc
+
+zap update all
